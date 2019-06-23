71f120a1332bf888ede329c8e727e493f7ad6b0f
Gabriel Granata <ggran067@uottawa.ca>
2019-06-23 15:34:35 -0400

    adds User, Posts, & Review routes & models

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError   = require('http-errors');
+const bodyParser    = require('body-parser');
+const express       = require('express');
+const path          = require('path');
+const cookieParser  = require('cookie-parser');
+const logger        = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter   = require('./routes/index');
+const postRouter    = require('./routes/posts');
+const reviewRouter  = require('./routes/reviews');
 
 const app = express();
 
@@ -20,7 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/posts', postRouter);
+app.use('/posts/:id/reviews', reviewRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
